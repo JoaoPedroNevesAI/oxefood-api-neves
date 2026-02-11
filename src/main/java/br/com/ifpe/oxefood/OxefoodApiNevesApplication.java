e23700dfe06863aae19cd75e82ebcdb043ef0cd2
aluno <aluno@ifpe>
2026-02-11 20:50:27 -0300

    Enviando arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiNevesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiNevesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiNevesApplication.class, args);
+	}
+
+}
